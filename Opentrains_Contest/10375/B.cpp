518eb03eedc4811a32cf502a697c9935ec9d0114
ytz12345 <1217783313@qq.com>
2019-05-27 07:03:20 -0400

    updated contest 10375

--- Opentrains_Contest/10375/B.cpp
+++ Opentrains_Contest/10375/B.cpp
@@ -4,12 +4,76 @@ using namespace std;
 
 const int SIZE = 8000000;
 
+const char *fg = "---###";
+
 char buf[SIZE];
 
 char text[5000000];
 
+int len_t;
+
+string s[1010];
+
+int n, ans[1010];
+
 int main() {
 	ios::sync_with_stdio(false);
-	fread(buf, 1, SIZE, stdin);
-	
+	int len = fread(buf, 1, SIZE, stdin);
+	int now;
+	for (now = 0; now < len; now ++) {
+		if (buf[now] == ' ' || buf[now] == '\n') continue;
+		if (buf[now] == '-' && buf[now] + 75 <= len) {
+			int flag = 1;
+			for (int i = 0, j = now; i < 12; i ++, j ++) {
+				if (fg[i % 6] != buf[j]) {
+					flag = 0;
+					break;
+				}
+			}
+			if (flag) break;
+		} 
+		text[len_t ++] = buf[now];
+	}
+	while (now < len) {
+		now += 75;
+		for (; now < len; now ++) {
+			if (buf[now] == ' ' || buf[now] == '\n' || buf[now] == '#') continue;
+			if (buf[now] == '-' && buf[now] + 75 <= len) {
+				int flag = 1;
+				for (int i = 0, j = now; i < 12; i ++, j ++) {
+					if (fg[i % 6] != buf[j]) {
+						flag = 0;
+						break;
+					}
+				}
+				if (flag) break;
+			} 
+			s[n] += buf[now];
+		}
+		n ++;
+	}
+
+	now = 0;
+	for (int i = 1; i <= n; i ++) 
+		for (int j = 0; j < n; j ++) {
+			int tmp = now, flag = 0, t = 0, l = s[j].size();
+			for (t = 0; tmp < len && t < l; tmp ++) {
+				if (text[tmp] == s[j][t]) t ++;
+				if (tmp - now > 100) {
+					flag = t >= 80;
+					break;
+				}
+			}
+			if (flag) {
+				ans[j] = i;
+				for (now = tmp; tmp < len && t < l; now ++)
+					if (text[now] == s[j][t]) 
+						t ++;
+				break;
+			}
+		}
+
+	for (int i = 0; i < n; i ++)
+		cout << ans[i] << ' ';
+	return 0;
 }
--- Opentrains_Contest/10375/J.cpp
+++ Opentrains_Contest/10375/J.cpp
@@ -4,96 +4,62 @@ using namespace std;
 
 const int N = 1e5 + 5;
 
-int n, m, q;
+int n, m;
 
 vector <int> e[N];
 
-int root[N], fa[N];
+int f[N];
 
-int siz[N], top[N], son[N], dep[N];
+int tim, vis[N], dis[N], fr[N];
 
-int tim, flag[N];
+int q[N], head, tail;
 
-void bfs(int rt) {
-	static int q[N];
-	static int head, tail, u;
+int getf(int x) {
+	return x == f[x] ? x : (f[x] = getf(f[x]));
+}
+
+void solve(int u, int v) {
+	static int now;
+	tim ++;
+	vis[u] = vis[v] = tim;
+	dis[u] = dis[v] = 0;
+	fr[u] = u, fr[v] = v;
 	head = 1, tail = 0;
-	q[++ tail] = rt, root[rt] = rt;
-	while (head <= tail) {
-		u = q[head ++];
-		siz[u] = 1;
-		for (int v : e[u]) {
-			if (!root[v]) {
-				root[v] = rt, fa[v] = u, dep[v] = dep[u] + 1, q[++ tail] = v;
+	q[++ tail] = u;
+	q[++ tail] = v;
+	while (1) {
+		now = q[head ++];
+		for (int t : e[now]) {
+			if (vis[t] == tim) {
+				if (fr[t] == fr[now]) continue;
+				printf("%d\n", dis[t] + dis[now] + 1);
+				return;
 			}
- 		}
-	}
-	for (int j, i = tail; i > 1; i --) {
-		j = q[i];
-		siz[fa[j]] += siz[j];
-	}
-	for (int j, i = tail; i > 0; i --) {
-		j = q[i];
-		if (siz[j] > siz[son[fa[j]]]) son[fa[j]] = j;
-		//printf("%d %d %d %d\n", i, j, fa[j], son[fa[j]]);
-	}
-	for (int j, i = 1; i <= tail; i ++) {
-		if (!top[j = q[i]])
-			for (; j != 0; top[j] = q[i], j = son[j]);
-	}
-}
-
-int dis(int u, int v) {
-	int d = 0;
-	for (int fu = top[u], fv = top[v]; fu != fv; fu = top[u = fa[fu]], d ++) {
-		if (dep[fu] < dep[fv]) swap(u, v), swap(fu, fv);
-		d += dep[u] - dep[fu];
-	}
-	//printf("%d %d %d\n", u, v, d);
-	return d + abs(dep[u] - dep[v]);
-}
-
-int bfs_dis(int u, int v, int max_dis) {
-	static int q[N], d[N];
-	static int head, tail, now;
-	head = 1, tail = 0;
-	q[++ tail] = u; tim ++; flag[u] = tim; d[u] = 0;
-	while (head <= tail) {
-		now = q[head];
-		if (d[head] > max_dis) return max_dis;
-		if (now == v) return d[head];
-		for (int w : e[now]) {
-			if (flag[w] != tim) {
-				flag[w] = tim, q[++ tail] = w, d[tail] = d[head] + 1;
+			else {
+				vis[t] = tim;
+				dis[t] = dis[now] + 1;
+				fr[t] = fr[now];
+				q[++ tail] = t;
 			}
 		}
-		head ++;
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin >> n >> m;
-	for (int u, v, i = 1; i <= m; i ++) {
+	for (int i = 1; i <= n; i ++) f[i] = i;
+	for (int u, v, i = 0; i < m; i ++) {
 		cin >> u >> v;
 		e[u].push_back(v);
 		e[v].push_back(u);
+		f[getf(v)] = getf(u);
 	}
-	for (int i = 1; i <= n; i ++) 
-		if (!root[i]) 
-			bfs(i);
-	/*for (int i = 1; i <= n; i ++)
-		printf("%d %d %d %d %d %d\n", i, dep[i], fa[i], siz[i], son[i], top[i]);
-	return 0;*/
-	cin >> q;
-	for (int w, u, v, i = 1; i <= q; i ++) {
+	cin >> m;
+	for (int u, v, i = 0; i < m; i ++) {
 		cin >> u >> v;
-		printf(" %d %d %d ", i, q, (i <= q ? 1 : 0));
-		if (root[u] != root[v]) {puts("-1"); continue;}
-		w = dis(u, v); //return 0; 
-		//printf("%d\n",w);
-		if (w == abs(dep[u] - dep[v])) printf("%d\n", w);
-		else printf("%d\n", bfs_dis(u, v, w));
+		if (getf(u) != getf(v)) puts("-1");
+		else solve(u, v);
 	}
- 	return 0;
+	return 0;
 }
